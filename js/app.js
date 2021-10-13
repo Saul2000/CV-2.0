695ac2e29b5e5fd7bfc9663c80aa93d7cf0f3698
Saú Gutiérrez Hernández <90923888+Saul2000@users.noreply.github.com>
2021-10-13 17:27:38 -0500

    V5

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ check.addEventListener('click', language);
 function language(){
     let id=check.checked;
     if (id==true){
-        location.href="/es/index.html";
+        location.href="es/index.html";
     } else {
         location.href="https://saul2000.github.io/CV-2.0/";
     }
