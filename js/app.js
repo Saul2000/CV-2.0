e878550f70d8e1fd0323a770a41f2850aa515832
Saú Gutiérrez Hernández <90923888+Saul2000@users.noreply.github.com>
2021-10-13 16:59:10 -0500

    V5

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ check.addEventListener('click', language);
 function language(){
     let id=check.checked;
     if (id==true){
-        location.href="es/index.html";
+        location.href="C:/Users/saulg/Documents/DECIMO/Portfolio/es/index.html";
     } else {
-        location.href="https://saul2000.github.io/CV-2.0/";
+        location.href="C:/Users/saulg/Documents/DECIMO/Portfolio/index.html";
     }
 }
